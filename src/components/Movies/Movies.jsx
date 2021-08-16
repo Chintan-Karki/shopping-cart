5e42d4b75d1f1f2aa0b2e0cd551622fb8eeb05d7
Chintan Karki <karkichintan2000@gmail.com>
2021-08-16 16:35:02 +0545

    useEffect corrected

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -8,36 +8,34 @@ import Movie from "./Movie/Movie";
 import { loadTmdbMovies } from "../../redux/Shopping/shopping-actions";
 
 const Products = ({ movies, loadMovies }) => {
-	const [dataSet, setDataSet] = useState([]);
+	const [query, setQuery] = useState("");
 	const [value, setValue] = useState("");
-	const [query, setQuery] = useState(value);
 
 	useEffect(() => {
-		requestMovies();
-	}, [query, setQuery]);
+		async function requestMovies() {
+			const API_KEY = process.env.REACT_APP_API_KEY;
+			const res = await fetch(
+				`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${
+					query === "" ? "Avengers" : query
+				}`
+			);
+			const json = await res.json();
+			const moviesData = json.results;
 
-	async function requestMovies() {
-		const API_KEY = process.env.REACT_APP_API_KEY;
-		const res = await fetch(
-			`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${
-				query === "" ? "Avengers" : query
-			}`
-		);
-		const json = await res.json();
-		const moviesData = await json.results;
-		const newDataSet = [];
-		moviesData.forEach((item) => {
-			newDataSet.push({
-				id: item.id,
-				title: item.original_title,
-				description: item.overview,
-				image: `https://image.tmdb.org/t/p/original${item.poster_path}`,
-				price: 10.0,
+			let newDataSet = [];
+			moviesData.forEach((item) => {
+				newDataSet.push({
+					id: item.id,
+					title: item.original_title,
+					description: item.overview,
+					image: `https://image.tmdb.org/t/p/original${item.poster_path}`,
+					price: 10.0,
+				});
 			});
-		});
-		setDataSet([...newDataSet]);
-		loadMovies(newDataSet);
-	}
+			loadMovies(newDataSet);
+		}
+		requestMovies();
+	}, [query]);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -52,7 +50,9 @@ const Products = ({ movies, loadMovies }) => {
 					name="name"
 					value={value}
 					placeholder="e.g. Avengers"
-					onChange={(e) => setValue(e.target.value)}
+					onChange={(e) => {
+						setValue(e.target.value);
+					}}
 				/>
 				<input type="submit" value="Submit" />
 			</form>
@@ -65,6 +65,7 @@ const Products = ({ movies, loadMovies }) => {
 						color="#79531a"
 						height={100}
 						width={100}
+						timeout={3000}
 					/>
 				)}
 			</div>
@@ -73,7 +74,6 @@ const Products = ({ movies, loadMovies }) => {
 };
 
 // export default Products;
-
 const mapStateToProps = (state) => {
 	return {
 		movies: state.shop.movies,
